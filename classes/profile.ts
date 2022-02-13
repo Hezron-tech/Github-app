1345627f4e5bb4e3499e004d1e7afce19297f34a
Hezron-tech <kiprophez@gmail.com>
2022-02-13 20:32:51 +0300

    adding profile html

--- classes/profile.ts
+++ classes/profile.ts
@@ -1,13 +1,12 @@
 export class Profile {
 
-    constructor( 
-        public login:string,
-        public avatar_url:string,
-        public bio:string,
-        public followers:number,
-        public following:number,
-        public public_repos:number
-    ){
-
-    }
+    constructor(
+        public avatar_url: string,
+        public login: string,
+        public bio: string,
+        public followers: number,
+        public following: number,
+        public public_repos: number
+    ) {
+  }
 }
